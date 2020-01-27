12619c855124144d66b2f3e2feeed485cdd573c0
David Swiatkiewiez <davidswiatkiewiez@MacBook-Pro-de-David-Swiatkiewiez.local>
2020-01-28 00:21:21 +0100

    reorganise le projet pour deploiement

--- src/App.js
+++ src/App.js
@@ -1,223 +1,29 @@
-import React, {Component} from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'bootstrap/dist/css/bootstrap.min.css';
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
-import logobleu from './images/logo bleu.png';
-import logorouge from './images/logo rouge.png';
+require('dotenv').config();
 
+const middlewares = require('./middlewares');
+const api = require('./api');
 
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
-import {Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+const app = express();
 
-import Joi from 'joi';
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-import L from "leaflet";
-import './App.css';
-
-var userIcon = L.icon({
-  iconUrl: logobleu,
-  iconSize: [36, 41],
-  iconAnchor: [18, 41],
-  popupAnchor: [0, -41],
+app.get('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+  });
 });
 
-var otherIcon = L.icon({
-  iconUrl: logorouge,
-  iconSize: [36, 41],
-  iconAnchor: [18, 41],
-  popupAnchor: [0, -41],
-});
+app.use('/api/v1', api);
 
-const schema = Joi.object({
-    nameSpot: Joi.string()
-        .min(1)
-        .max(100)
-        .required(),
-    message: Joi.string()
-        .min(1)
-        .max(500)
-        .required(),
-})
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
-const API_URL = window.location.hostname == 'localhost' ? 'http://localhost:5000/api/v1/messages': 'https://myfavspots.herokuapp.com/api/v1/messages';
-
-class App extends Component {
-
-  state = {
-    position:{
-      lat: 51.505,
-      lng: -0.09
-    },
-    zoom: 2,
-    gotUserLocation:false,
-    newSpot:{
-      nameSpot:"",
-      message:""
-    },
-    sendingMessage: false,
-    sentMessage: false,
-    messages: []
-  };
-
-  componentDidMount() {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then( messages =>{
-        this.setState({
-          messages
-        })
-      })
-
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({
-        position:{
-          lat : position.coords.latitude,
-          lng : position.coords.longitude
-        },
-        gotUserLocation:true,
-        zoom:13
-      })
-    }, () => {
-      fetch("https://ipapi.co/json")
-      .then(res => res.json())
-      .then(location => {
-        this.setState({
-        position:{
-          lat : location.latitude,
-          lng : location.longitude
-        },
-        gotUserLocation:true,
-        zoom:13
-        })
-      })
-    })
-  }
-
-formIsValid() {
-  const newSpot = {
-    nameSpot : this.state.newSpot.nameSpot,
-    message : this.state.newSpot.message
-  }
-
-  const result = Joi.validate(newSpot, schema);
-
-  return !result.error && this.state.gotUserLocation ? true : false;
-}
-
-  onSubmit = (e) =>  {
-    e.preventDefault();
-    this.setState({
-      sendingMessage: true
-    })
-    if(this.formIsValid()) {
-      fetch(API_URL, {
-        method: 'POST',
-        headers: {
-          'content-type' : 'application/json'
-        },
-        body: JSON.stringify({
-          nameSpot : this.state.newSpot.nameSpot,
-          message : this.state.newSpot.message,
-          latitude : this.state.position.lat,
-          longitude : this.state.position.lng
-        })
-      }).then( res => res.json())
-        .then(message => {
-          console.log(message)
-          setTimeout( () => {
-            this.setState({
-              sendingMessage: false,
-              sentMessage: true
-            })
-          }, 2000);
-        })
-    }
-  }
-
-  changeValue = (e) => {
-     const {name, value} = e.target;
-     this.setState(prevState => ({
-       newSpot:{
-         ...prevState.newSpot,
-         [name] :value
-       }
-     }))
-  }
-
-  render() {
-    const position = [this.state.position.lat, this.state.position.lng];
-
-    return (
-      <div className="App">
-        <Map className="map" center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {
-            this.state.gotUserLocation ?
-            <Marker position={position} icon={userIcon}>
-            </Marker> : ''
-          }
-          {
-            this.state.messages.map(message => (
-              <Marker
-                key={message._id}
-                position={[message.latitude, message.longitude] }
-                icon={otherIcon}>
-                <Popup>
-                  <em>{message.nameSpot}</em> : {message.message}
-                </Popup>
-              </Marker> : ''
-            ))
-          }
-          }
-        </Map>
-        <Card body className="formulaire">
-          <CardTitle>myFavSpots</CardTitle>
-          <CardText>You've find a new cool spot, and you want to remember it, you've come to the right spot!</CardText>
-          {!this.state.sendingMessage && !this.state.sentMessage && this.state.gotUserLocation ?
-            <Form   onSubmit={this.onSubmit}>
-              <FormGroup>
-                <Label for="nameSpot">Spot</Label>
-                <Input
-                  onChange={this.changeValue}
-                  type="text"
-                  name="nameSpot"
-                  id="nameSpot"
-                  placeholder="What's the spot?"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="message">Message</Label>
-                <Input
-                  onChange={this.changeValue}
-                  type="textarea"
-                  name="message"
-                  id="message"
-                  placeholder="Give a comment"
-                />
-              </FormGroup>
-              <Button
-                color="info"
-                type="submit"
-                disabled={!this.formIsValid()}
-              >Send
-              </Button>
-            </Form> :
-            this.state.sendingMessage || !this.state.gotUserLocation ?
-            <img alt="loading..." src="https://i.giphy.com/media/3oEjHTSuJrMnj08DpS/giphy.webp" /> :
-            <CardText>Nice, You've got a new favorite spot!</CardText>
-          }
-
-        </Card>
-      </div>
-    )
-  }
-}
-export default App;
-
-
-//3'31'00 du tuto
-// deploy with heroku
-// penser au database url une fois sur heroku
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const app = require('./app');
 
-import leaflet from 'leaflet/dist/leaflet.css';
-
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
